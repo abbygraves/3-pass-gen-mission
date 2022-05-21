ccd4f7b792ac3722ae39f3a5de99149015795651
abbygraves <abbygraves14@gmail.com>
2022-05-20 22:41:47 -0500

    password length function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,43 @@
 // Assignment code here
 
 
+var charSet = "";
+var passLength = 0;
+var lowercase = "acdefghijklmnnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNNOPQRSTUVWXYZ";
+var numeric = "123456789";
+//var special =
+
+
+
+var passLength = function() { 
+  passLength = window.prompt("How many characters would you like to include in your password? Please choose a number between 8 and 128!");
+  if (passLength < 8 || passLength > 128) {
+    window.alert("enter vaild");
+  };
+};
+
+var passCriteria = function () {
+  lowercase = window.confirm("Would you like to include lowercase letters?")
+  if (lowercase = true) {
+    console.log(lowercase);
+  }
+};
+
+
+
+var generatePassword = function () {
+  passLength(),
+  passCriteria();
+};
+
+
+
+
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +52,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
